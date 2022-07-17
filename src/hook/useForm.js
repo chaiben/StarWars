437efb30115317dbdef077a08b85a3720d62537f
Marçal <chaiben79@gmail.com>
2022-07-17 14:10:30 +0200

    Fixed validation form

--- src/hook/useForm.js
+++ src/hook/useForm.js
@@ -1,103 +1,109 @@
-import { CHECKEMAIL, CHECKPASSWORD2 } from "../def"
-import useLocalStorage from "./useLocalStorage"
+import { CHECKEMAIL, CHECKPASSWORD2 } from "../def";
+import useLocalStorage from "./useLocalStorage";
 
-export default function useForm(formName, initialFormData){
-  const [formData, setFormData] = useLocalStorage(`${formName}-formData`, initialFormData)
-  const [errorFormData, setErrorFormData] = useLocalStorage(`${formName}-errorFormData`, '')
+export default function useForm(formName, initialFormData) {
+  const [formData, setFormData] = useLocalStorage(
+    `${formName}-formData`,
+    initialFormData
+  );
+  const [errorFormData, setErrorFormData] = useLocalStorage(
+    `${formName}-errorFormData`,
+    ""
+  );
 
   function handleChange(event) {
-    const {name, value, type, checked} = event.target
+    const { name, value, type, checked } = event.target;
     setFormData({
       ...formData,
-      [name]: type === "checkbox" ? checked : value
-    })
+      [name]: type === "checkbox" ? checked : value,
+    });
     setErrorFormData({
       ...errorFormData,
-      [name]: ""
-    })
+      [name]: "",
+    });
   }
 
-  function validFormField(element, save){
-    const {name, value, required, placeholder} = element;
-    let errorFormDataAux = {}
-
-    if(required && value !==''){
+  function validFormField(element, save) {
+    const { name, value, required, placeholder } = element;
+    let errorFormDataAux = {};
+    if (required && value === "") {
       errorFormDataAux = {
         ...errorFormData,
-        [name]: ""
-      } 
-    } else { 
+        [name]: placeholder
+          ? "Please enter your " + placeholder.toLowerCase()
+          : "This field is required",
+      };
+    } else {
       errorFormDataAux = {
         ...errorFormData,
-        [name]: (placeholder) ? 
-         "Please enter your " + placeholder.toLowerCase() :
-         "This field is required"
-      }
+        [name]: "",
+      };
     }
-    if(value){
+    if (value) {
       switch (name) {
-        case 'email':
-          errorFormDataAux = (value.match(CHECKEMAIL)) ? 
-            {
-              ...errorFormData,
-              [name]: ""
-            } : {
-              ...errorFormData,
-              [name]: "Invalid email format"
-            }
+        case "email":
+          errorFormDataAux = value.match(CHECKEMAIL)
+            ? {
+                ...errorFormData,
+                [name]: "",
+              }
+            : {
+                ...errorFormData,
+                [name]: "Invalid email format",
+              };
           break;
-        case 'password':
-          errorFormDataAux = (value.match(CHECKPASSWORD2)) ? 
-          {
-            ...errorFormData,
-            [name]: ""
-          } : 
-          {
-            ...errorFormData,
-            [name]: `Your password must have:
+        case "password":
+          errorFormDataAux = value.match(CHECKPASSWORD2)
+            ? {
+                ...errorFormData,
+                [name]: "",
+              }
+            : {
+                ...errorFormData,
+                [name]: `Your password must have:
             Minimum of 8 characters;
             Maximun of 16 characters;
-            A minimum of 1 lower case letter,  1 upper case letter, 1 numeric character and 1 special character. Good luck!`
-          }
+            A minimum of 1 lower case letter,  1 upper case letter, 1 numeric character and 1 special character. Good luck!`,
+              };
           break;
-      
+
         default:
           break;
       }
     }
-    if(save !== false)
-      setErrorFormData(errorFormDataAux)
-  
+    if (save !== false) setErrorFormData(errorFormDataAux);
+
     return errorFormDataAux;
   }
-  function validateAllForm(){
-    let errorFormDataAux = {}
-    Object.keys(formData).forEach(elementName => {
+  function validateAllForm() {
+    let errorFormDataAux = {};
+    Object.keys(formData).forEach((elementName) => {
       const element = document.getElementById(elementName);
       errorFormDataAux = {
         ...errorFormDataAux,
-        ...validFormField(element, false)
-      }
+        ...validFormField(element, false),
+      };
     });
     setErrorFormData(errorFormDataAux);
-    
+    console.log(errorFormData);
+
     // Check if there is any error enabled
-    const errorValues = Object.values(errorFormDataAux)
+    const errorValues = Object.values(errorFormDataAux);
     for (let errorValue of errorValues) {
-      if(errorValue){
-        return false
+      if (errorValue) {
+        return false;
       }
     }
-    return true
+    return true;
   }
 
   return [
-    formData, 
-    errorFormData, 
-    handleChange, 
+    formData,
+    errorFormData,
+    handleChange,
     validFormField,
     validateAllForm,
-    setFormData, 
+    setFormData,
     setErrorFormData,
-  ]
+  ];
 }
